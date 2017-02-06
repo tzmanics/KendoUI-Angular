97ac28bf07b3dc274bbbe7b170dffdb2135763b1
tzmanics <tzmanics@gmail.com>
2017-02-06 13:51:33 -0500

    add custom styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ '../../node_modules/@progress/kendo-theme-default/dist/all.css' ]
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
   title = 'Making an Angular App';
