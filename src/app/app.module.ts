a6f8e3d56c281eb595efbc9f13e8e8ff1baa80bc
tzmanics <tzmanics@gmail.com>
2017-02-06 14:19:31 -0500

    add new test component & styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TestStyleComponent } from './test-style/test-style.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestStyleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test-style/test-style.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-style',
+  templateUrl: './test-style.component.html',
+  styleUrls: ['./test-style.component.scss']
+})
+export class TestStyleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
